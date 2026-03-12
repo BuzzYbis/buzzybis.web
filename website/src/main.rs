634d98031180f32ff038456de9f0e6f28086a137
ugo <ugo.gosso@ecole2600.com>
2026-03-12 14:44:34 +0100

    add sidebar colapse

--- website/src/main.rs
+++ website/src/main.rs
@@ -74,9 +74,42 @@ fn App() -> Element {
 
 #[component]
 fn EngLayout() -> Element {
+    let mut is_collapsed = use_signal(|| false,);
+
     rsx! {
         div { class: "eng-layout",
-            nav { class: "eng-sidebar",
+            button {
+                class: if is_collapsed() { "toggle-btn collapsed" } else { "toggle-btn" },
+                // Flip the boolean when clicked
+                onclick: move |_| is_collapsed.toggle(),
+
+                // Change the icon/text based on state
+                if is_collapsed() {
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: "20",
+                        height: "20",
+                        fill: "currentColor",
+                        class: "bi bi-layout-sidebar",
+                        view_box: "0 0 16 16",
+                        path { d: "M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z" }
+                    }
+                } else {
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: "20",
+                        height: "20",
+                        fill: "currentColor",
+                        class: "bi bi-layout-sidebar",
+                        view_box: "0 0 16 16",
+                        path { d: "M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z" }
+                    }
+                }
+            }
+
+            nav {
+                class: if is_collapsed() { "eng-sidebar collapsed" } else { "eng-sidebar" },
+
                 Link {
                     class: "eng-brand",
                     to: Route::EngHome {}, "BuzzY_"
